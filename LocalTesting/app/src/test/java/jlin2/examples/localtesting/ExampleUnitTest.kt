92fb8f49ab9751c97df45903a596ca4b095fd2c2
Dharini Panchal 1191207 <d_panchal209666@fanshaweonline.ca>
2024-08-01 16:32:23 -0400

    Generated 3 testing on email
    - fomal email id fomat 123@abc.com
    - incorrect email id, no domain: 123@abc
    - correct format but i extra dot brefore domain: 123@abc..com

--- LocalTesting/app/src/test/java/jlin2/examples/localtesting/ExampleUnitTest.kt
+++ LocalTesting/app/src/test/java/jlin2/examples/localtesting/ExampleUnitTest.kt
@@ -4,14 +4,19 @@ import org.junit.Test
 
 import org.junit.Assert.*
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun email_isCorrect() {
+        assertTrue(EmailValidator.isValidEmail("123@abc.com"))
+    }
+
+    @Test
+    fun email_isIncorrect(){
+        assertFalse(EmailValidator.isValidEmail("123@abc"))
+    }
+
+    @Test
+    fun email_Dots_isIncorrect(){
+        assertFalse(EmailValidator.isValidEmail("123@abc..com"))
     }
 }
